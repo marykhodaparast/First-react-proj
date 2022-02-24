b7f0858ed086c4c6635395ae30421956e50be8e5
maryam <50619466+marykhodaparast@users.noreply.github.com>
2022-02-24 16:16:44 +0330

    Create todo component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Todo from "./components/Todo";
 function App() {
-  return <div>Hello!</div>
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn React"/>
+      <Todo text="Master React"/>
+      <Todo text="Something else"/>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,19 @@
+function Todo(props) {
+  function deleteHandler() {
+    //console.log('Clicked');
+    console.log(props.text);
+  }
+  return (
+    <div>
+      <div className="card">
+        <h2>{props.text}</h2>
+        <div className="actions">
+          <button className="btn" onClick={deleteHandler}>
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Todo;
